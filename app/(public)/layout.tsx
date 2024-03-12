ffbed53ea27fb817586316da169b0b11e81bc2b9
Guilherme-Jardim <guilhermejardimj@gmail.com>
2024-03-12 14:39:17 -0300

    products style component

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import "../globals.css";
 import NavBar from "../components/NavBar";
+import clsx from 'clsx';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({
+  subsets: ['latin']
+});
 
 export const metadata: Metadata = {
   title: "e-commerce",
@@ -10,11 +16,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-Br">
-      <body>
-        <main >
+      <body className={clsx(inter.className, 'bg-slate-700')}>
+        <NavBar />
+        <main className="h-screen p-16">
           {children}
         </main>
       </body>
-    </html>
+    </html >
   );
 }
