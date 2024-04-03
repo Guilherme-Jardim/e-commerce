3f9ec05699cc728bab9d53f380705f2503ee0d7e
Guilherme-Jardim <guilhermejardimj@gmail.com>
2024-04-03 19:14:53 -0300

    prisma postgresql config

--- app/(public)/register/page.tsx
+++ app/(public)/register/page.tsx
@@ -1,18 +1,50 @@
+'use client'
+import { SyntheticEvent, useState } from "react";
+import { Prisma } from "@prisma/client";
+
 export default function Register() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: ""
+  });
+
+  async function handleSubmit(e: SyntheticEvent) {
+    e.preventDefault();
+
+    try {
+      // Crie um novo usuário no banco de dados usando o Prisma Client
+      const newUser = await Prisma.create({
+        data: {
+          name: formData.name,
+          email: formData.email,
+          password: formData.password
+        }
+      });
+
+      console.log("Novo usuário criado:", newUser);
+
+      // Limpe o formulário após a criação do usuário
+      setFormData({ name: "", email: "", password: "" });
+    } catch (error) {
+      console.error("Erro ao criar usuário:", error);
+    }
+  }
+
   return (
     <div className="min-h-screen flex-grow flex flex-col justify-center items-center">
       <header>
         <h1>Registro</h1>
       </header>
       <main className="">
-        <form /*onSubmit={handleSubmit}*/ className="w-full max-w-md p-8">
+        <form onSubmit={handleSubmit} className="w-full max-w-md p-8">
           <label htmlFor="name" className="block mb-2">Nome</label>
           <input
             type="text"
             id="name"
             name="name"
-            // value={formData.name}
-            //  onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+            value={formData.name}
+            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
             className="w-full border border-gray-300 rounded-md p-2"
           />
           <label htmlFor="email" className="block mb-2 mt-4">Email</label>
@@ -20,8 +52,8 @@ export default function Register() {
             type="email"
             id="email"
             name="email"
-            // value={formData.email}
-            //  onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+            value={formData.email}
+            onChange={(e) => setFormData({ ...formData, email: e.target.value })}
             className="w-full border border-gray-300 rounded-md p-2"
           />
           <label htmlFor="password" className="block mb-2 mt-4">Senha</label>
@@ -29,8 +61,8 @@ export default function Register() {
             type="password"
             id="password"
             name="password"
-            // value={formData.password}
-            // onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+            value={formData.password}
+            onChange={(e) => setFormData({ ...formData, password: e.target.value })}
             className="w-full border border-gray-300 rounded-md p-2"
           />
           <button type="submit" className="bg-blue-500 text-white w-full p-2 mt-4 rounded-md">
@@ -41,6 +73,6 @@ export default function Register() {
       <footer>
         <p>Copyright &copy; 2024</p>
       </footer>
-    </div >
+    </div>
   );
 }
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -45,3 +45,116 @@ const handler = NextAuth({
 })
 
 export { handler as GET, handler as POST }
+
+
+
+
+
+
+
+
+// import NextAuth, { NextAuthOptions } from 'next-auth'
+// import GitHubProvider, { GithubProfile } from "next-auth/providers/github";
+// import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
+// import CredentialsProvider, { CredentialsProfile } from "next-auth/providers/credentials";
+// import { PrismaAdapter } from '../../../lib/auth/prisma-adapter'
+// import { NextApiRequest, NextApiResponse } from 'next'
+
+// export function buildNextAuthOptions(
+//   req: NextApiRequest,
+//   res: NextApiResponse,
+// ): NextAuthOptions {
+//   return {
+//     providers: [
+//       GitHubProvider({
+//         clientId: process.env.GITHUB_ID!,
+//         clientSecret: process.env.GITHUB_SECRET!,
+//         authorization: {
+//           params: {
+//             scope:
+//               'read:user user:email',
+//           },
+//         },
+//         profile(profile: GithubProfile) {
+//           return {
+//             id: profile.id.toString(),
+//             name: profile.name ?? '',
+//             username: profile.login ?? '',
+//             email: profile.email ?? '',
+//             avatar_url: profile.avatar_url ?? '',
+//           }
+//         },
+//       }),
+//       GoogleProvider({
+//         clientId: process.env.GOOGLE_CLIENT_ID!,
+//         clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+//         authorization: {
+//           params: {
+//             scope:
+//               'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
+//           },
+//         },
+//         profile(profile: GoogleProfile) {
+//           return {
+//             id: profile.sub,
+//             name: profile.name ?? '',
+//             username: '',
+//             email: profile.email ?? '',
+//             avatar_url: profile.picture ?? '',
+//           }
+//         },
+//       }),
+//       CredentialsProvider({
+//         name: "Credentials",
+//         credentials: {
+//           username: { label: "Username", type: "text", placeholder: "jsmith" },
+//           password: { label: "Password", type: "password" }
+//         },
+//         async authorize(credentials, req) {
+//           const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+    
+//           if (user) {        
+//             return user
+//           } else {
+//             return null
+//           }
+//         },
+//         authorization: {
+//           params: {            
+//           },
+//         },
+//         profile(profile: CredentialsProfile) {
+//           return {
+//             id: profile.sub,
+//             name: profile.name ?? '',
+//             username: '',
+//             email: profile.email ?? '',
+//             avatar_url: profile.picture ?? '',
+//           }
+//         },
+//       }),
+//     ],
+//     callbacks: {
+//       async signIn({ account }) {
+//         if (
+//           !account?.scope?.includes('https://www.googleapis.com/auth/userinfo.profile')
+//         ) {
+//           return '/register/connect-calendar/?error=permissions'
+//         }
+//         return true
+//       },
+//     },
+//     pages: {
+//       signIn: '/', // Página de login
+//       signOut: '/perfil', // Página de logout
+//       error: '/auth/error', // Página de erro (código de erro passado na string de consulta como ?error=)
+//       verifyRequest: '/auth/verify-request', // Usado para verificar o email
+//       newUser: '/auth/new-user' // Novos usuários serão redirecionados aqui na primeira entrada (deixe a propriedade de fora se não for do interesse)
+//     },
+//     adapter: PrismaAdapter(req, res),
+//   }
+// }
+
+// export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+//   return await NextAuth(req, res, buildNextAuthOptions(req, res))
+// }
