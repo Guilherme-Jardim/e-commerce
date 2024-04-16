8ed3be3bd3fbe970447ed4e9814b931163792ce3
Guilherme-Jardim <guilhermejardimj@gmail.com>
2024-04-16 20:12:41 -0300

    nextauth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,100 +1,49 @@
-import { AuthOptions } from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "../../../lib/prisma";
+import { PrismaClient } from "@prisma/client";
 
-let userAccount;
+const prisma = new PrismaClient();
 
-const authOptions: AuthOptions = {
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-    CredentialsProvider({
-      credentials: {
-        email: { label: "Email", type: "text", placeholder: "Email" },
-        password: { label: "Password", type: "password" },
+      clientId: process.env.GOOGLE_CLIENT_ID !,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET !,
+      async profile(profile) {
+        // Verifica se o ID do perfil está presente
+        if (!profile.id) {
+          throw new Error("Profile id is missing in Google OAuth profile response");
+        }
+
+        return {
+          id: profile.id.toString(), // Converte o ID para uma string
+          name: profile.name || profile.login,
+          email: profile.email,
+          image: profile.avatar_url,
+        };
       },
-      async authorize(credentials, req) {
-        const email = credentials?.email;
-        const password = credentials?.password;
-
-        if (!email || !password) {
-          return null;
-        }
-
-        const res = await fetch(
-          `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api/user/login`,
-          {
-            method: "POST",
-            body: JSON.stringify({ email, password }),
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        );
-        const user = await res.json();
-
-        if (res.ok && user) {
-          userAccount = user;
-          console.log(userAccount);
-          return user;
-        } else {
-          return null;
-        }
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
       },
     }),
   ],
-
-  adapter: PrismaAdapter(prisma),
-  secret: process.env.NEXTAUTH_SECRET,
-  session: { strategy: "jwt", maxAge: 24 * 60 * 60 },
-
-  jwt: {
-    secret: process.env.NEXTAUTH_SECRET,
-    maxAge: 60 * 60 * 24 * 30,
-  },
-
-  pages: {
-    signIn: "/perfil",
-    signOut: "/",
-    error: "/auth/error",
-  },
-
   callbacks: {
-    async session(params: {
-      session: any; // Ou ajuste para o tipo correto se possível
-      token: any; // Ou ajuste para o tipo correto se possível
-      user: any; // Ou ajuste para o tipo correto se possível
-      newSession: any; // Ou ajuste para o tipo correto se possível
-      trigger: "update";
-    }): Promise<any> {
-      const { session, user, token, newSession, trigger } = params;
-    
-      // Sua lógica de sessão aqui
-    
-      return session;
-    },
-    
-
-    async jwt({ token, user }) {
-      const isSignedIn = user ? true : false;
-
-      if (isSignedIn) {
-        token.accessToken =
-          user.id.toString() + "-" + user.email + "-" + user.name;
+    async signIn({ user, account, profile, email, credentials }) {
+      const isAllowedToSignIn = true;
+      if (isAllowedToSignIn) {
+        return true;
+      } else {
+        return false;
       }
-
-      return await token;
     },
   },
-};
+});
 
-export default authOptions;
+// Exporta o handler para lidar com solicitações GET e POST
+export { handler as GET, handler as POST };
--- app/components/login/MenuSigInUp.tsx
+++ app/components/login/MenuSigInUp.tsx
@@ -1,109 +1,17 @@
+'use client'
 import { signIn } from "next-auth/react";
-import Link from "next/link";
-import { useState, useEffect } from "react";
 
-export default function MenuSigInUp() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const [emailInputError, setEmailInputError] = useState(false);
-  const [passwordInputError, setPasswordInputError] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    const validate = () => {
-      let emailIsValid = validateEmail(email);
-
-      if (!emailIsValid) {
-        setEmailInputError(true);
-      } else {
-        setEmailInputError(false);
-      }
-
-      if (password.length < 6) {
-        setPasswordInputError(true);
-      } else {
-        setPasswordInputError(false);
-      }
-    };
-
-    validate();
-  }, [email, password]);
-
-  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    setIsLoading(true);
-
-    let res = await signIn("credentials", {
-      email,
-      password,
-      callbackUrl: `/perfil`,
-      redirect: false,
-    });
-
-    setIsLoading(false);
-
-    if (res?.ok) {
-      console.log("success");
-    } else {
-      setError("Failed! Check you input and try again.");
-      console.log("Failed", res);
-    }
-  }
-
-  function validateEmail(email: string) {
-    return email.includes('@');
-  }
-
-  if (isLoading) {
-    return <p className="flex h-full">Loading...</p>;
-  }
+export default function MenuSignInUp() {
 
   return (
-    <div className="p-4 bg-slate-900">
-      <h2 className="text-lg font-medium text-white">Login</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          name="email"
-          type="email"
-          placeholder="Email"
-          className={`w-full border-b text-slate-950 border-gray-200 mb-4 p-2 ${emailInputError ? "border-red-500" : ""}`}
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          name="password"
-          type="password"
-          placeholder="Senha"
-          className={`w-full border-b text-slate-950 border-gray-200 mb-4 p-2 ${passwordInputError ? "border-red-500" : ""}`}
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button
-          type="submit"
-          className="bg-slate-500 text-white w-full p-2 rounded-md"
-        >
-          Entrar
-        </button>
-      </form>
-      <p className="mt-2 text-sm text-white">Esqueci minha senha</p>
-      <hr className="my-4 bg-white" />
-      <p className="text-sm text-white">Ainda não tem conta?</p>
-      <Link href="/register">Registre-se</Link>
-      <div>
-        <button
-          className="bg-slate-500 text-white w-full p-2 rounded-md"
-          onClick={() => signIn("github", { callbackUrl: "/perfil" })}
-        >
-          Login com GitHub
-        </button>
-        <button
-          className="bg-slate-500 text-white w-full p-2 rounded-md"
-          onClick={() => signIn("google", { callbackUrl: "/perfil" })}
-        >
-          Login com Google
-        </button>
-      </div>
+    <div className="p-4 bg-slate-900 flex items-center justify-center">
+      <button
+        className="bg-slate-500 text-white w-full p-2 rounded-md flex"
+        onClick={() => signIn("google", { callbackUrl: "/perfil" })}
+      >
+        Login com Google
+      </button>
+
     </div>
   );
 }
